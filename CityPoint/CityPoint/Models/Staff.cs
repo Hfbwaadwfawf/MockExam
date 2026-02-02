7e85301b4fddaff010098642455c40819b978699
Hfbwaadwfawf <rexxxits@gmail.com>
2026-02-02 11:09:58 +0000

    added VS project

---
+++ CityPoint/CityPoint/Models/Staff.cs
@@ -0,0 +1,10 @@
+﻿namespace CityPoint.Models
+{
+    public class Staff
+    {
+        public int StaffId { get; set; }
+        public string Name { get; set; }
+        public string Role { get; set; }
+        public string Bio {  get; set; }
+    }
+}
